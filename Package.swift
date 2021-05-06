415f40e514a6e94ab7576403ffa6e51ad78f8bf5
Patryk Średziński <patryk.sredzinski@iteo.com>
2021-05-06 12:09:21 +0200

    Exclude Logger preview images

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
             path: "IteoLogger",
             exclude: [
                 "Info.plist",
+                "Preview/"
             ],
             resources: [
                 .process("Media.xcassets/")
